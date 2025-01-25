21afbf2ef6179532ce669ffcb560599b20219efc
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-24 19:23:06 -0600

    Create classes

---
+++ HerenciaQ12025/Invertebrado.cs
@@ -0,0 +1,27 @@
+﻿
+namespace HerenciaQ12025
+{
+    public class Invertebrado : Animal
+    {
+        public string? Tipo { get; set; }
+        public bool TienePatas { get; set; }
+        public int NumeroPatas { get; set; }
+        public bool TieneConcha { get; set; }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Tamaño: {Tamano}");
+            Console.WriteLine($"Familia: {Familia}");
+            Console.WriteLine($"Tiene Patas: {(TienePatas ? "Si" : "No")}");
+
+            if (TienePatas)
+            {
+                Console.WriteLine($"Número de Patas: {NumeroPatas}");
+            }
+
+            Console.WriteLine($"Tiene Concha: {(TieneConcha ? "Si" : "No")}");
+        }
+    }
+}
