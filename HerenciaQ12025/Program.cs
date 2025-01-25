e2b7bfd1a65d1b5604c44de4ae30d145edecb780
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-24 19:23:16 -0600

    Adding data

--- HerenciaQ12025/Program.cs
+++ HerenciaQ12025/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HerenciaQ12025;
+
+Vertebrado vertebrado = new Vertebrado();
+Invertebrado invertebrado = new Invertebrado();
+int opcion;
+string? tienePatas, esDomestico, esHerbivoro, esCarnivoro, tieneConcha;
+
+Console.WriteLine("****************************");
+Console.WriteLine("*           MENU           *");
+Console.WriteLine("* 1. Animal Vertebrado     *");
+Console.WriteLine("* 2. Animal Invertebrado   *");
+Console.WriteLine("****************************");
+Console.WriteLine("Escoja la opción:");
+opcion = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+switch (opcion)
+{
+    case 1:
+        Console.WriteLine("Escriba el nombre:");
+        vertebrado.Nombre = Console.ReadLine();
+
+        Console.WriteLine("Escriba el color:");
+        vertebrado.Color = Console.ReadLine();
+
+        Console.WriteLine("Escriba el tamaño:");
+        vertebrado.Tamano = Console.ReadLine();
+
+        Console.WriteLine("Escriba la Familia:");
+        vertebrado.Familia = Console.ReadLine();
+
+        Console.WriteLine("¿Tiene patas? (s/n)");
+        tienePatas = Console.ReadLine();
+
+        if (tienePatas.ToLower() == "s")
+        {
+            vertebrado.TienePatas = true;
+        }
+
+        Console.WriteLine("¿Es doméstico? (s/n)");
+        esDomestico = Console.ReadLine();
+
+        if (esDomestico.ToLower() == "s")
+        {
+            vertebrado.EsDomestico = true;
+        }
+
+        Console.WriteLine("¿Es herbívoro? (s/n)");
+        esHerbivoro = Console.ReadLine();
+
+        if (esHerbivoro.ToLower() == "s")
+        {
+            vertebrado.EsHerbivoro = true;
+        }
+
+        Console.WriteLine("¿Es carnívoro? (s/n)");
+        esCarnivoro = Console.ReadLine();
+
+        if (esCarnivoro.ToLower() == "s")
+        {
+            vertebrado.EsCarnivoro = true;
+        }
+
+        vertebrado.Imprimir();
+        break;
+
+    case 2:
+        Console.WriteLine("Escriba el nombre:");
+        vertebrado.Nombre = Console.ReadLine();
+
+        Console.WriteLine("Escriba el color:");
+        vertebrado.Color = Console.ReadLine();
+
+        Console.WriteLine("Escriba el tamaño:");
+        vertebrado.Tamano = Console.ReadLine();
+
+        Console.WriteLine("Escriba la Familia:");
+        vertebrado.Familia = Console.ReadLine();
+
+        break;
+
+    default:
+        Console.WriteLine("Opción no válida");
+        break;
+}
