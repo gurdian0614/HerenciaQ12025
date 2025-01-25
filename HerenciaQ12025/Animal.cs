8ae62ba8119056df3da14300d36992ddcb74edce
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-24 19:22:55 -0600

    Create father class

---
+++ HerenciaQ12025/Animal.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HerenciaQ12025
+{
+    public class Animal
+    {
+        public string? Nombre { get; set; }
+        public string? Color { get; set; }
+        public string? Tamano { get; set; }
+        public string? Familia { get; set; }
+    }
+}
